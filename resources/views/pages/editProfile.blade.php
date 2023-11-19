ce57e7105d20db4d141ff965ff8ddc40393fad8a
migueljcpedrosa <migueljcpedrosa@gmail.com>
2023-11-19 16:33:29 +0000

    editProfile created

---
+++ resources/views/pages/editProfile.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.appLogged')
+
+@section('title', 'Edit Profile')
+
+@section('content')
+
+<!-- Edit Profile Section -->
+<section id="edit-profile" class="edit-profile-section">
+    <div class="container">
+        <h1>Edit Profile</h1>
+        <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            
+            <!-- Profile Picture -->
+            <div class="form-group">
+                <label for="profile_picture">Profile Picture</label>
+                <input type="file" name="profile_picture" id="profile_picture" class="form-control">
+            </div>
+
+            <!-- Name -->
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}">
+            </div>
+
+            <!-- Email -->
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}">
+            </div>
+
+            <!-- Username -->
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" class="form-control" value="{{ $user->username }}">
+            </div>
+
+            <!-- Phone Number -->
+            <div class="form-group">
+                <label for="phone_number">Phone Number</label>
+                <input type="text" name="phone_number" id="phone_number" class="form-control" value="{{ $user->phone_number }}">
+            </div>
+
+            <!-- Birthdate -->
+            <div class="form-group">
+                <label for="birthdate">Birthdate</label>
+                <input type="date" name="birthdate" id="birthdate" class="form-control" value="{{ $user->birthdate }}">
+            </div>
+
+            <!-- Description -->
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" class="form-control">{{ $user->description }}</textarea>
+            </div>
+
+            <!-- Submit Button -->
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Update Profile</button>
+            </div>
+        </form>
+    </div>
+</section>
+
+@endsection
