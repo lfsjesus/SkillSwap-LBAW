5555d91d6e5119bfcb6f61ca34cc91b060d1362c
r00cha <roocha99pt@gmail.com>
2023-11-20 22:58:19 +0000

    Restyled edit profile

--- resources/views/pages/editProfile.blade.php
+++ resources/views/pages/editProfile.blade.php
@@ -13,49 +13,49 @@
             @method('PUT')
             
             <!-- Profile Picture -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="profile_picture">Profile Picture</label>
                 <input type="file" name="profile_picture" id="profile_picture" class="form-control">
             </div>
 
             <!-- Name -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name" class="form-control" value="{{ $user->name }}">
             </div>
 
             <!-- Email -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" class="form-control" value="{{ $user->email }}">
             </div>
 
             <!-- Username -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" class="form-control" value="{{ $user->username }}">
             </div>
 
             <!-- Phone Number -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="phone_number">Phone Number</label>
                 <input type="text" name="phone_number" id="phone_number" class="form-control" value="{{ $user->phone_number }}">
             </div>
 
             <!-- Birthdate -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="birthdate">Birthdate</label>
                 <input type="date" name="birth_date" id="birthdate" class="form-control" value="{{ $user->birth_date->format('d/m/Y') }}">
             </div>
 
             <!-- Description -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="description">Description</label>
                 <textarea name="description" id="description" class="form-control">{{ $user->description }}</textarea>
             </div>
 
             <!-- Submit Button -->
-            <div class="form-group">
+            <div id="form-group">
                 <button type="submit" class="btn btn-primary">Update Profile</button>
             </div>
         </form>
