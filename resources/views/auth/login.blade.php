044b729c317c678d62d6e04023b2fa0c3297b7e0
migueljcpedrosa <migueljcpedrosa@gmail.com>
2023-11-20 22:13:40 +0000

    Updated login form to include continue as guest

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,7 @@
     <div class="auth-form">
         <h1> Welcome back! </h1>
         @include('partials.login-form')
+        <a href="{{ route('home') }}"> Continue as Guest</a>
     <div>
 </div>
 @endsection
