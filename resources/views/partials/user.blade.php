988a814dc74ce124838cba88df471c7a022f7e48
r00cha <roocha99pt@gmail.com>
2023-11-20 16:40:33 +0000

    Styled user card

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -2,3 +2,15 @@
     <h3>{{ $user->name }}</h3>
     <p>{{ $user->email }}</p>
 </div>
+
+<div class="user-card">
+    <a href="{{ route('user', ['username' => $user->username]) }}">
+        @if($user->profile_picture)
+        <img src="{{stream_get_contents($user->profile_picture)}}"/>
+        @else
+        <img src="{{ url('assets/profile-picture.png') }}"/>
+        @endif
+        {{ $user->name }} | {{$user->username}}
+    </a>
+
+</div>
