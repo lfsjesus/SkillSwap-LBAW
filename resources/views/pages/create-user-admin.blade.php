676c22b5ac4bded0078604a6d982b3011f153e0e
r00cha <roocha99pt@gmail.com>
2023-11-20 23:12:51 +0000

    Small fixes in forms

--- resources/views/pages/create-user-admin.blade.php
+++ resources/views/pages/create-user-admin.blade.php
@@ -12,61 +12,61 @@
             {{ csrf_field() }}
             @method('POST')
             <!-- Profile Picture -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="profile_picture">Profile Picture</label>
                 <input type="file" name="profile_picture" id="profile_picture" class="form-control">
             </div>
 
             <!-- Name -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name" class="form-control">
             </div>
 
             <!-- Email -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" class="form-control">
             </div>
 
             <!-- Username -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" class="form-control">
             </div>
 
             <!-- Password -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" class="form-control">
             </div>
 
             <!-- Password Confirmation -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="password_confirmation">Password Confirmation</label>
                 <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
             </div>
 
             <!-- Phone Number -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="phone_number">Phone Number</label>
                 <input type="text" name="phone_number" id="phone_number" class="form-control">
             </div>
 
             <!-- Birthdate -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="birthdate">Birthdate</label>
                 <input type="text" name="birth_date" id="birthdate" class="form-control">
             </div>
 
             <!-- Description -->
-            <div class="form-group">
+            <div id="form-group">
                 <label for="description">Description</label>
                 <textarea name="description" id="description" class="form-control"></textarea>
             </div>
 
             <!-- Submit Button -->
-            <div class="form-group">
+            <div id="form-group">
                 <button type="submit" class="btn btn-primary">Create Profile</button>
             </div>
         </form>
