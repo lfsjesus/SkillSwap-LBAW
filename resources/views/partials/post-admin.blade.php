de1944c1dd7f8d3a3cc7c9180608ac0ce5bf25eb
r00cha <roocha99pt@gmail.com>
2023-11-28 11:59:28 +0000

    Added user profile links in user posts

--- resources/views/partials/post-admin.blade.php
+++ resources/views/partials/post-admin.blade.php
@@ -1,15 +1,23 @@
 <article class="post" data-id="{{ $post->id }}">
     <div class="post-header">
         <div class="post-header-left">
+            <a href="{{ route('view-user-admin', ['username' => $post->author->username]) }}">
             @if($post->author->profile_picture) 
             <img src="{{stream_get_contents($post->author->profile_picture)}}"/>
             @else
             <img src="{{ url('assets/profile-picture.png') }}"/>
             @endif
+            </a>
 
             <div class="author-date">
-                <p> {{$post->author->name}} </p>
+                <a class="flex" href="{{ route('view-user-admin', ['username' => $post->author->username]) }}">
+                    <p> {{$post->author->name}} </p>
+                    <span class="username">
+                        &#64;{{$post->author->username}}
+                    </span>
+                </a>    
                 <p> {{$post->date->format('F j, Y, g:i a')}} </p>
+                
             </div>
         </div>
         <div class="post-header-right">
