ab8922f09a87aaba9a246de5cbf58005ca4f5a05
LuAs Filipe da Silva Jesus <up202108683@fe.up.pt>
2023-12-16 19:59:42 +0000

    Revert "Merge branch 'contextual-help' into 'main'"
    This reverts merge request !59

--- resources/views/pages/editGroup.blade.php
+++ resources/views/pages/editGroup.blade.php
@@ -14,13 +14,7 @@
             <input type="hidden" name="id" value="{{ $group->id }}">
             <!-- Banner -->
             <div id="form-group">
-                <div class="field-title">
-                    <label for="banner">Banner</label>
-                    <span class="help-icon material-symbols-outlined"> info </span>
-                    <div class="help-tooltip">
-                        Accepted formats: jpg, png. Max size: 2MB.
-                    </div>
-                </div>
+                <label for="banner">Banner</label>
                 <input type="file" name="banner" id="banner" class="form-control">
                 @if ($errors->has('banner'))
                 <span class="error">
@@ -31,13 +25,7 @@
 
             <!-- Name -->
             <div id="form-group">
-                <div class="field-title">
-                    <label for="name">Name</label>
-                    <span class="help-icon material-symbols-outlined"> info </span>
-                    <div class="help-tooltip">
-                        Write the group's name here.
-                    </div>
-                </div>
+                <label for="name">Name</label>
                 <input type="text" name="name" id="name" class="form-control" value="{{ $group->name }}" required>
                 @if ($errors->has('name'))
                 <span class="error">
@@ -48,13 +36,7 @@
 
             <!-- Description -->
             <div id="form-group">
-                <div class="field-title">
-                    <label for="name">Description</label>
-                    <span class="help-icon material-symbols-outlined"> info </span>
-                    <div class="help-tooltip">
-                        Write a brief description about the group.
-                    </div>
-                </div>
+                <label for="description">Description</label>
                 <textarea name="description" id="description" class="form-control">{{ $group->description }}</textarea>
                 @if ($errors->has('description'))
                 <span class="error">
@@ -65,13 +47,7 @@
 
             <!-- Visibility -->
             <div id="form-group">
-                <div class="field-title">
-                    <label for="visibility">Visibility</label>
-                    <span class="help-icon material-symbols-outlined"> info </span>
-                    <div class="help-tooltip">
-                        Select 'Public' for everyone to view your details, or 'Private' for limited access.
-                    </div>
-                </div>
+                <label for="visibility">Visibility</label>
                 <select name="visibility" id="visibility" class="form-control">
                     <option value="1" {{ $group->public_group ? 'selected' : '' }}>Public</option>
                     <option value="0" {{ $group->public_group ? '' : 'selected' }}>Private</option>
