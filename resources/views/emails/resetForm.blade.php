829d6937fd24a965345e0773c401e19f27f12ce3
migueljcpedrosa <migueljcpedrosa@gmail.com>
2023-12-18 20:03:17 +0000

    Improved recover password form

--- resources/views/emails/resetForm.blade.php
+++ resources/views/emails/resetForm.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container recover-password-page">
     <h2>Reset Password</h2>
-    
+
     @if ($errors->any())
-    <div class="alert alert-danger">
+    <div class="error">
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
@@ -13,17 +13,19 @@
         </ul>
     </div>
     @endif
-
-    <form method="POST" action="{{ route('send') }}">
-        @csrf
-        
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input id="email" type="email" name="email" value="{{ old('email') }}" required placeholder="E-mail" autofocus>
-        </div>
-        
-        <button type="submit" class="btn btn-primary">Send</button>
-    </form>
+    <div class="auth-form">
+        <form method="POST" action="{{ route('send') }}">
+            @csrf
+            
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" required placeholder="E-mail" autofocus>
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Send</button>
+        </form>
+    </div>
+   
 </div>
 @endsection
 
